9190659cd7f5d652e740026a117197469877e366
NancyHamdallah <nana_beauty1@hotmail.com>
2025-04-30 03:45:22 +0300

    CI/CD with Docker image

--- src/main/java/com/example/editorwithpayload/EditorwithpayloadApplication.java
+++ src/main/java/com/example/editorwithpayload/EditorwithpayloadApplication.java
@@ -1,5 +1,6 @@
 package com.example.editorwithpayload;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EditorwithpayloadApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		// Set required environment variables
+		System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
+		System.setProperty("GITHUB_CLIENT_ID", dotenv.get("GITHUB_CLIENT_ID"));
+		System.setProperty("GITHUB_CLIENT_SECRET", dotenv.get("GITHUB_CLIENT_SECRET"));
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+		System.setProperty("SUPABASE_URL", dotenv.get("SUPABASE_URL"));
+		System.setProperty("SUPABASE_API_KEY", dotenv.get("SUPABASE_API_KEY"));
+		System.setProperty("SUPABASE_BUCKET_NAME", dotenv.get("SUPABASE_BUCKET_NAME"));
+		System.setProperty("JDOODLE_CLIENT_ID", dotenv.get("JDOODLE_CLIENT_ID"));
+		System.setProperty("JDOODLE_CLIENT_SECRET", dotenv.get("JDOODLE_CLIENT_SECRET"));
+
 		SpringApplication.run(EditorwithpayloadApplication.class, args);
 	}
+
 
 }
