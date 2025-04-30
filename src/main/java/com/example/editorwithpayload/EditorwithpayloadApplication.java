c9b890d0bbfc69b66ba3c56f87affb284185998e
NancyHamdallah <nana_beauty1@hotmail.com>
2025-04-30 13:20:30 +0300

    prepare variables for deployment

--- src/main/java/com/example/editorwithpayload/EditorwithpayloadApplication.java
+++ src/main/java/com/example/editorwithpayload/EditorwithpayloadApplication.java
@@ -8,20 +8,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EditorwithpayloadApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		if (System.getenv("RAILWAY_STATIC_URL") == null) {
+			Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
-		// Set required environment variables
-		System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
-		System.setProperty("GITHUB_CLIENT_ID", dotenv.get("GITHUB_CLIENT_ID"));
-		System.setProperty("GITHUB_CLIENT_SECRET", dotenv.get("GITHUB_CLIENT_SECRET"));
-		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
-		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
-		System.setProperty("SUPABASE_URL", dotenv.get("SUPABASE_URL"));
-		System.setProperty("SUPABASE_API_KEY", dotenv.get("SUPABASE_API_KEY"));
-		System.setProperty("SUPABASE_BUCKET_NAME", dotenv.get("SUPABASE_BUCKET_NAME"));
-		System.setProperty("JDOODLE_CLIENT_ID", dotenv.get("JDOODLE_CLIENT_ID"));
-		System.setProperty("JDOODLE_CLIENT_SECRET", dotenv.get("JDOODLE_CLIENT_SECRET"));
-
+			// Set required environment variables
+			System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
+			System.setProperty("GITHUB_CLIENT_ID", dotenv.get("GITHUB_CLIENT_ID"));
+			System.setProperty("GITHUB_CLIENT_SECRET", dotenv.get("GITHUB_CLIENT_SECRET"));
+			System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+			System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+			System.setProperty("SUPABASE_URL", dotenv.get("SUPABASE_URL"));
+			System.setProperty("SUPABASE_API_KEY", dotenv.get("SUPABASE_API_KEY"));
+			System.setProperty("SUPABASE_BUCKET_NAME", dotenv.get("SUPABASE_BUCKET_NAME"));
+			System.setProperty("JDOODLE_CLIENT_ID", dotenv.get("JDOODLE_CLIENT_ID"));
+			System.setProperty("JDOODLE_CLIENT_SECRET", dotenv.get("JDOODLE_CLIENT_SECRET"));
+		}
 		SpringApplication.run(EditorwithpayloadApplication.class, args);
 	}
 
